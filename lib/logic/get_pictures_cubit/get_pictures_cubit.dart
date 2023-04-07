b9f4c23bb0885e9062583660ddc3bf189563e888
adilet <ibraevadilet3@gmail.com>
2023-04-07 15:55:21 +0600

    add

---
+++ lib/logic/get_pictures_cubit/get_pictures_cubit.dart
@@ -0,0 +1,48 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:re_fotos/logic/model/image_model.dart';
+
+part 'get_pictures_cubit.freezed.dart';
+
+class GetPicturesCubit extends Cubit<GetPicturesState> {
+  GetPicturesCubit() : super(const GetPicturesState.initial());
+
+  Future<void> getPictures() async {
+    emit(const GetPicturesState.loading());
+    try {
+      final pictures = await getPicturesFromServer();
+      pictures.shuffle();
+      emit(GetPicturesState.success(pictures));
+    } catch (e) {
+      emit(GetPicturesState.failed(e.toString()));
+    }
+  }
+
+  Future<List<ImageModel>> getPicturesFromServer() async {
+    final snapshot =
+        await FirebaseDatabase.instance.ref('retouch_lifehacks').get();
+
+    final lessonsFromServer = <ImageModel>[];
+    if (snapshot.value != null) {
+      final map = snapshot.value as Map<dynamic, dynamic>;
+
+      map.forEach((key, value) {
+        value['id'] = key;
+        final lesson = ImageModel.fromJson(value);
+
+        lessonsFromServer.add(lesson);
+      });
+    }
+
+    return lessonsFromServer;
+  }
+}
+
+@freezed
+class GetPicturesState with _$GetPicturesState {
+  const factory GetPicturesState.initial() = _Initial;
+  const factory GetPicturesState.loading() = _Loading;
+  const factory GetPicturesState.success(List<ImageModel> pictures) = _Success;
+  const factory GetPicturesState.failed(String message) = _Failed;
+}
---
+++ lib/logic/model/image_model.dart
@@ -0,0 +1,25 @@
+class ImageModel {
+  late final String title;
+  late final String image;
+  late final String longDescription;
+
+  ImageModel({
+    required this.title,
+    required this.image,
+    required this.longDescription,
+  });
+
+  ImageModel.fromJson(Map<dynamic, dynamic> json) {
+    title = json['title'];
+    image = json['image'];
+    longDescription = json['long_description'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['title'] = title;
+    data['image'] = image;
+    data['long_description'] = longDescription;
+    return data;
+  }
+}
